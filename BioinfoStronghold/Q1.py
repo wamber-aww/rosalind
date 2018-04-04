5504d0d655c8b30e8f755f2602eac81a63507f64
Amber <32796435+wamber-aww@users.noreply.github.com>
2018-04-03 20:21:21 -0400

    Delete Q1.py

--- BioinfoStronghold/Q1.py
+++
@@ -1,9 +0,0 @@
-def countNt(string):
-    nt = {}
-    for i in string:
-        if i not in nt:
-            nt[i] = 1
-        else:
-            nt[i] += 1
-    
-    print nt['A'], nt['C'], nt['G'], nt['T']
